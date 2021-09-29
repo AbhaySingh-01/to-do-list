6c4328d6be96e68f7d54a537dd9dccaaef8dd7bf
Abha <abhayr48@gmail.com>
2021-09-30 01:08:50 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,61 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from 'react'
+// import {MdDelete} from 'react-icons/md'
+// import {GrAddCircle} from 'react-icons/gr'
+import './App.css'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+function App(){
+
+ const[list, setList] = useState([])
+   
+ const [input, setInput] =  useState('')
+    function addItem(e){
+        
+        setInput(e.target.value);
+    }
+    function pushItem(){
+        if(input==''){
+
+        }
+        else {
+           setList([...list, {
+               key: Math.floor(Math.random()*10),
+               value: input
+            }])
+        }
+    }
+    function removeItem(key){
+        const newAr = list.filter((item) => item.key !== key);
+               setList(newAr)
+    }
+    const toDoList  = list.map((item)=>
+    <li className="listItem">{item.value} <button className="del-btn" onClick={()=> removeItem(item.key)}>Delete</button>
+    </li>)
+return (
+<div className="container-fluid">
+    <div className="row top-empty-row">
     </div>
-  );
+    <div className=" row functional-section">
+        <div className="col-lg-3"></div>
+        <div className=" col-lg-6 main-section">
+            <div className="row section-header">
+                <div className="col-lg-12 section-title"> TO DO LIST </div>
+                </div>
+                <div className="row section-input">
+                        <input type="text" className="task-input" placeholder="Add Tasks..." onChange={addItem} /> 
+                    <button onClick={pushItem} className="add-btn"> Add </button>
+                 </div>
+<div className="row section-list">
+    <ul className="list">
+    {toDoList}
+    </ul>
+    </div>
+</div> {/* main-section */}
+<div className="col-lg-3"></div>
+</div>
+ <div className="row"></div>
+</div>
+
+);
 }
 
-export default App;
+export default App
